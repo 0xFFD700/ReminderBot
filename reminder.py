d6112dca8060f0347fd332380f4e2664f500dcb7
Jen <j.hofbauer223@t-online.de>
2021-05-22 01:16:00 +0200

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -7,10 +7,10 @@ currentDate = date.today().strftime("%A").lower()
 with open('reminder.csv', 'r') as file:
 	reader = csv.reader(file, delimiter=';')
 	for row in reader:
-		if (row[1] == currentDate or row[1] == "everyday"):
-			if(row[0][0] == '+49'):
-				print('signal-cli -u phonenumber send -m "' + row[2] + '" ' + row[0])
+		if (row[2] == currentDate or row[2] == "everyday"):
+			if(row[1] == "person"):
+				print('signal-cli -u phonenumber send -m "' + row[3] + '" ' + row[0])
 				os.system('signal-cli -u +49<TelephonenumberSender> send -m "' + row[2] + '" ' + row[0])
 			else:
-				print('signal-cli -u phonenumber send -m "' + row[2] + '" -g ' + row[0])
+				print('signal-cli -u phonenumber send -m "' + row[3] + '" -g ' + row[0])
 				os.system('signal-cli -u +49<TelephonenumberSender> send -m "' + row[2] + '" -g ' + row[0])
